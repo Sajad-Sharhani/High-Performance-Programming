b85d8355e847b23e231c633b3eab6598657bffb0
sash0017 <sajad.sharhani.0017@student.uu.se>
2023-02-07 12:41:39 +0100

    A2 update

--- Assignments/Assignment2/A2/part3/january.c
+++ Assignments/Assignment2/A2/part3/january.c
@@ -1,141 +1,181 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Defining the structure of the linked list*/
+// defining the node structure
 typedef struct node
 {
-    int index; // index value the day of january(1-31)
-    float min_temp;
-    float max_temp;
-    struct node *next;
+    int index;         // index value the day of january(1-31)
+    float min_temp;    // minimum temperature for the day
+    float max_temp;    // maximum temperature for the day
+    struct node *next; // pointer to the next node
 } node_t;
-/*Function that prints the linked list*/
-void printList(node_t *head)
+
+// head pointer to the linked list
+node_t *head = NULL;
+
+// function to print the linked list
+void print_list()
 {
-    node_t *temp = head;
-    temp = temp->next;
+    // current pointer to the head of the linked list
+    node_t *current = head;
+
     printf("Day     min     max\n");
-    /*looping through all the linked nodes and printing their values*/
-    while (temp != NULL)
+    // traversing the linked list and printing the values
+    while (current != NULL)
     {
-        printf("%d      %f     %f\n", temp->index, temp->min_temp, temp->max_temp);
-        temp = temp->next;
+        printf("%d      %f     %f\n", current->index, current->min_temp, current->max_temp);
+        current = current->next;
     }
-    free(temp);
     printf("\n");
 }
-/*Function which creates a new node and allocates memory for it */
-node_t *newNode(int new_index, float new_min_temp, float new_max_temp)
+
+// function to insert a node into the linked list in a sorted manner with no duplicates
+void insert(int index, float min_temp, float max_temp)
 {
+    // current pointer to the head of the linked list
+    node_t *current = head;
+    // previous pointer to the previous node
+    node_t *previous = NULL;
+
+    // creating a new node
     node_t *new_node = (node_t *)malloc(sizeof(node_t));
-
-    new_node->index = new_index;
-    new_node->min_temp = new_min_temp;
-    new_node->max_temp = new_max_temp;
+    new_node->index = index;
+    new_node->min_temp = min_temp;
+    new_node->max_temp = max_temp;
     new_node->next = NULL;
-    return new_node;
-}
-/*Function which inserts a node in the linked list*/
-void insert(node_t *head, node_t *new)
-{
-    node_t *temp = head;
-    /*searching the linked list to find the location to insert the node
-    in the location which will keep the linked list sorted by index value*/
-    while (temp->next != NULL && temp->next->index <= new->index)
-    {
-        temp = temp->next;
-    }
-    /*case where a node has the same index as the inserting node*/
-    if (temp->index == new->index)
-    {
-        temp->min_temp = new->min_temp; // We update the existing
-        temp->max_temp = new->max_temp; // node with the new min
-        free(new);                      // and max temp and then remove the new node from memory
-        return;
-    }
-    /* case where the node with the specific index does not exist in the linked list  */
-    else
-    {
-        new->next = temp->next;
-        temp->next = new;
 
-        return;
-    }
-}
-/*function to delete the specified node based on its index value*/
-void delete(node_t *head, int index)
-{
-    node_t *temp = head, *previous;
-    /*searching in the linked list until we find a node with the specified index*/
-    while (temp->next != NULL && temp)
-    {
-        previous = temp;
-        temp = temp->next;
-        if (temp->index == index)
-        {
-            previous->next = temp->next; // updating the linked list
-            free(temp);                  // and removing the node from memory
-            return;
-        }
-    }
-}
-
-int main()
-{
-    node_t *head = NULL;                     // initializing the linked list
-    head = (node_t *)malloc(sizeof(node_t)); // allocating memory for the header of the linked list
-    int index;
-    double min, max;
-    char command;
+    // checking if the linked list is empty
     if (head == NULL)
     {
-        return 1;
+        // assigning the new node to the head of the linked list
+        head = new_node;
+        return;
     }
 
+    // traversing the linked list to find the location to insert the new node
+    while (current != NULL && current->index < index)
+    {
+        // assigning the current node to the previous node
+        previous = current;
+        // assigning the next node to the current node
+        current = current->next;
+    }
+
+    // checking if the new node has the same index as the current node
+    if (current != NULL && current->index == index)
+    {
+        // updating the current node with the new min and max temperatures
+        current->min_temp = min_temp;
+        current->max_temp = max_temp;
+        // freeing the memory allocated to the new node
+        free(new_node);
+        return;
+    }
+
+    // checking if the new node is to be inserted at the head of the linked list
+    if (previous == NULL)
+    {
+        // assigning the new node to the head of the linked list
+        head = new_node;
+        // assigning the next node to the next of the new node
+        new_node->next = current;
+    }
+    else
+    {
+        // assigning the new node to the next of the previous node
+        previous->next = new_node;
+        // assigning the next node to the next of the new node
+        new_node->next = current;
+    }
+}
+
+// function to delete the specified node based on its index value
+void delete(int index)
+{
+    // current pointer to the head of the linked list
+    node_t *current = head;
+    // previous pointer to the previous node
+    node_t *previous = NULL;
+
+    // traversing the linked list to find the node to be deleted
+    while (current != NULL && current->index < index)
+    {
+        // assigning the current node to the previous node
+        previous = current;
+        // assigning the next node to the current node
+        current = current->next;
+    }
+
+    // checking if the index value is not found in the linked list or the linked list is empty
+    if (current == NULL || current->index != index)
+    {
+
+        return;
+    } // checking if the node to be deleted is the head of the linked list
+    else if (previous == NULL)
+    {
+        // assigning the next node to the head of the linked list
+        head = current->next;
+    }
+    else
+    {
+        // assigning the next node to the next of the previous node
+        previous->next = current->next;
+    }
+
+    // freeing the memory allocated to the node to be deleted
+    free(current);
+}
+int main()
+{
+    char command;
+    int index;
+    float min_temp;
+    float max_temp;
+
+    // infinite loop to take the user input
     while (1)
     {
-        printf(" Enter command: ");
+        // taking the user input
+        printf("Enter command: ");
         scanf(" %c", &command);
+
+        // checking the user input
         switch (command)
         {
-        case 'A':
-        {
 
-            scanf(" %d %lf %lf", &index, &min, &max);
-            if (index < 1 || index > 31) // checking for the index to match the days of january
+        case 'A':
+            scanf(" %d %f %f", &index, &min_temp, &max_temp);
+            // checking for the index to match the days of january
+            if (index < 1 || index > 31)
             {
-                printf(" Invalid index! \r \n");
+                printf("Invalid index value. Please enter a value between 1 and 31.\n");
                 break;
             }
-            node_t *new = newNode(index, min, max);
-            insert(head, new);
+            insert(index, min_temp, max_temp);
             break;
-        }
+
         case 'D':
-        {
             scanf("%d", &index);
-            if (index < 1 || index > 31) // checking for the index to match the days of january
+            // checking for the index to match the days of january
+            if (index < 1 || index > 31)
             {
-                printf(" Invalid index! \r \n");
+                printf("Invalid index value. Please enter a value between 1 and 31.\n");
                 break;
             }
-            delete (head, index);
+            delete (index);
             break;
-        }
+
         case 'P':
-        {
-            printList(head);
+            print_list();
             break;
-        }
+
         case 'Q':
-        {
             return 0;
-        }
+
         default:
-        {
-            printf(" Invalid command! \r \n");
+            printf("Invalid command. Please enter a valid command.\n");
             break;
-        }
         }
     }
 }
